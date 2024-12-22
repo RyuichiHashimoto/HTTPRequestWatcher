1ac3725b57e420b07a310a6e64e1a5225654dfb8
hashimoto <ryuichi.hashimoto.git@gmail.com>
2024-12-22 12:42:36 +0900

    add hostname

--- entry-point.sh
+++ entry-point.sh
@@ -2,7 +2,7 @@
 
 # HTTP POSTリクエストを送信する関数
 send_webhook() {
-  local ip_addr="$1"  # IPアドレス
+  local hostname="$1"  # IPアドレス
   local url="$2"      # Webhook URL
   local _msg="$3"      # メッセージ内容
 
@@ -15,22 +15,17 @@ send_webhook() {
   curl -X POST -H "Content-Type: application/json" -d "$json_data" "$url"
 }
 
-url=$DISCORD_WEBHOOK_URL
-start_msg="[HTTPRequestWatcher] start aws instance in $ip_addr"
-
-# IPアドレス取得
-if command -v ec2-metadata > /dev/null 2>&1; then
-    ip_addr=$(ec2-metadata | grep public-ipv4 | awk '{print $2}')
-else
-    ip_addr=""
-fi
-
 # cronサービスを起動
 service cron start
 
+url=$DISCORD_WEBHOOK_URL
+hostname=$DOMAIN
+
+start_msg="[HTTPRequestWatcher] start aws instance in $hostname"
+
 # 関数を呼び出し
-send_webhook "$ip_addr" "$url" "start aws instance in ${ip_addr}"
-send_webhook "$ip_addr" "$url" "http://${ip_addr}:5010/register/sessionid"
-send_webhook "$ip_addr" "$url" "http://${ip_addr}:5010/show/sessionid"
+send_webhook "$hostname" "$url" "start aws instance in ${hostname}"
+send_webhook "$hostname" "$url" "http://${hostname}:5000/"
+send_webhook "$hostname" "$url" "http://${hostname}:5000/register/hogehoge"
 
 python app.py
